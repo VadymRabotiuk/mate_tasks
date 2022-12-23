ea9d31e4fbb3b8d863ca9cb9a84e93e7bdf4cc12
Vadym Rabotiuk <vadymrabotiuk@gmail.com>
2022-12-23 16:52:50 +0200

    get_outdated_robots with list com

---
+++ get_outdated_robots.py
@@ -0,0 +1,13 @@
+def get_outdated_robots(robots: list, new_version: int) -> list:
+    return [index for index, robot in enumerate(robots) if robot["core_version"] < new_version]
+
+
+robots = [
+        {"core_version": 9},
+        {"core_version": 13},
+        {"core_version": 16},
+        {"core_version": 9},
+        {"core_version": 14},
+    ]
+new_version = 10
+print(get_outdated_robots(robots, new_version))
